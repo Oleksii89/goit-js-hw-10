ae4f91a9737d9ff7da99d091586be59b83817388
Oleksii89 <108924918+Oleksii89@users.noreply.github.com>
2023-08-10 15:53:01 +0300

    update

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,7 +1,9 @@
 const BASE_URL = 'https://api.thecatapi.com/v1';
+const API_KEY =
+  'live_6pSKRboySWbbUVzhZK0GmvcHpXjZ2jwlVVY9nclqHJFk4LaUET5TjmfpBsT9kKPP';
 
 function fetchBreeds() {
-  return fetch(`${BASE_URL}/breeds`).then(resp => {
+  return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`).then(resp => {
     if (!resp.ok) {
       throw new Error(resp.statusText);
     }
@@ -13,12 +15,14 @@ function fetchCatByBreed(breedId) {
     breed_ids: breedId,
   });
 
-  return fetch(`${BASE_URL}/images/search?${params}`).then(resp => {
-    if (!resp.ok) {
-      throw new Error(resp.statusText);
+  return fetch(`${BASE_URL}/images/search?api_key=${API_KEY}&${params}`).then(
+    resp => {
+      if (!resp.ok) {
+        throw new Error(resp.statusText);
+      }
+      return resp.json();
     }
-    return resp.json();
-  });
+  );
 }
 
 export { fetchBreeds };
--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ refs.select.addEventListener('change', onSelectBreed);
 function onSelectBreed(event) {
   const breedId = event.currentTarget.value;
   fetchCatByBreed(breedId)
-    .then(data => console.log(data))
+    .then(data => createMarkup(data))
     .catch(err => console.log(err));
 }
 
@@ -34,7 +34,8 @@ function createList(arr) {
 function createMarkup(data) {
   const card = data
     .map(el => {
-      return `<li><img src="" alt="" width="" height="" /><h2></h2><p></p><h3>Temperament</h3><p></p></li>`;
+      return `<li><img src="${el.url}" alt="${el.breeds[0].name}" width="400"/><h2>${el.breeds[0].name}</h2><p>${el.breeds[0].description}</p><h3>Temperament</h3><p>${el.breeds[0].temperament}</p></li>`;
     })
     .join('');
+  refs.catInfo.innerHTML = card;
 }
